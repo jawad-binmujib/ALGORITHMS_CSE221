01b1b1fa860059c6c2a2446b048188726072ca06
jawad219 <jawadbinmujib219@gmail.com>
2025-10-23 01:23:00 +0600

    Add solution for task 04: Check if an array is sorted in ascending order
    - Implemented input handling for multiple test cases.
    - Created a vector to store the elements of the array.
    - Added logic to determine if the array is sorted in ascending order.
    - Output "YES" if sorted, otherwise "NO".

---
+++ assignment_01/task_04.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int test_cases; 
+    cin >> test_cases;
+    for (int i = 0 ; i < test_cases; i++) {
+        int length; 
+        cin >> length;
+        vector<int> arr1;
+
+        for (int i = 1; i <= length; i ++) {
+            int elem;
+            cin >> elem;
+            arr1.push_back(elem);
+        }
+        bool isAssend = true;
+        for (int i = 0; i < length - 1; i ++) {
+            if (arr1[i] > arr1[i + 1]){
+                isAssend = false;
+                break;
+            }
+        }
+
+        if (isAssend) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+
+    }
+
+    return 0;
+}
