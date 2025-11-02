90ad0d62f4b0863967be340a9ceccf4a083607a9
jawad219 <jawadbinmujib219@gmail.com>
2025-11-02 22:04:41 +0600

    refactor: replace NULL with nullptr for better type safety

--- assignment_02/task_08.cpp
+++ assignment_02/task_08.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
     int test_cases;
     cin >> test_cases;
     while (test_cases--) {
