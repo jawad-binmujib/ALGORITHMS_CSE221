16cc8e3ae24374804a20494f92f0ff0934ee7c56
jawad219 <jawadbinmujib219@gmail.com>
2025-11-04 16:35:30 +0600

    Implemented insertion_sort function

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int length;
+    cin >> length;
+    vector<int> arr1(length);
+    for (int i = 0; i < length; i++){
+        cin >> arr1[i];
+    }
+    for (int i = 1;i < length; i++){
+        int key = arr1[i];
+        int j = i - 1;
+        while (j >= 0 && arr1[j] > key)
+        {
+            arr1[j +1] = arr1[j];
+            j-= 1;
+        }
+        arr1[j + 1] = key;
+        
+    }
+
+    for (int elem : arr1){
+        cout << elem << " ";
+
+    }
+    cout << endl;
+
+
+    return 0;
+}
