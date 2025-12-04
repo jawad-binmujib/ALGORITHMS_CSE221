933196d533e0a6d14774072cbb7d054e15fa791d
jawad219 <jawadbinmujib219@gmail.com>
2025-12-04 13:20:19 +0600

    implementted codes for selection sort

---
+++ sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int selection_sort(vector<int> &arr1, int length){
+    for (int i = 0; i < length -1; i++){
+        int min_idx = i;
+        for (int j = i +1; j < length; j ++){
+            if (arr1[j] < arr1[min_idx]){
+                min_idx = j;
+            }
+        }
+        swap(arr1[i], arr1[min_idx]);
+    }
+    return 0;
+}
+int main(){
+    int length;
+    cin >> length;
+    vector<int> arr1(length);
+    for (int i = 0; i < length; i++){
+        cin >> arr1[i];
+    }
+    selection_sort(arr1, length);
+    for (int elem : arr1){
+        cout << elem << " ";
+    }
+    cout << endl;
+    return 0;
+}
