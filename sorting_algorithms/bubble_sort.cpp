8543fec82b999447e0bda941bb922b38b35e3459
jawad219 <jawadbinmujib219@gmail.com>
2025-12-04 23:51:12 +0600

    refactor bubble sort implementation for clarity and efficiency

--- sorting_algorithms/bubble_sort.cpp
+++ sorting_algorithms/bubble_sort.cpp
@@ -1,27 +1,28 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
 using namespace std;
-int main(){
-
-    int length;
-    cin >> length;
-    vector<int>arr1(length);
-    for (int i = 0; i < length; i++){
-        cin >> arr1[i];
-    }
-
-    for (int i = 0; i < length - 1; i++){
-        for (int j = 0; j < length - 1; j ++){
-            if (arr1[j] > arr1[j + 1]){
-                swap(arr1[j], arr1[j + 1]);
+int bubble_sort(vector<int> &arr1, int length){
+    for (int i =0; i < length; i++){
+        for (int j = 0 ; j < length; j++ ){
+            if (arr1[j] > arr1[j +1]){
+                swap(arr1[j], arr1[j+1]);
             }
         }
     }
-    for (int i : arr1){
-        cout << i << " " ;
-
+    return 0;
+}
+int main(){
+    int length;
+    cin >> length;
+    vector<int> arr1(length);
+    for (int i =0; i < length; i ++){
+        cin >> arr1[i];
     }
+    bubble_sort(arr1, length);
+    for (int elem  : arr1){
+        cout << elem << " ";
+    }
+
     return 0;
 }
