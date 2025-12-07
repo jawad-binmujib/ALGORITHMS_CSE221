ff52a1a7cb0e9dcdc5460228fa211aa661194ae3
jawad219 <jawadbinmujib219@gmail.com>
2025-12-08 03:10:18 +0600

    Add topological sort implementation for task_01

---
+++ assignment_06/task_01.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+const int max_number = 200005;
+vector<int> adj[max_number];      
+int in_degree[max_number];        
+vector<int> result;
+int main(){
+    int course, prereq;
+    cin >> course >> prereq;
+
+    for (int i = 0; i < prereq; i++){
+
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v); 
+        in_degree[v]++;
+    }
+    queue<int> q;
+    for (int i= 1; i <= course; i++){
+        if (in_degree[i] == 0)  q.push(i);
+    
+
+    }
+
+    while (!q.empty()){
+        int node = q.front();
+        q.pop();
+        result.push_back(node);
+
+        for (int v : adj[node]) {
+            in_degree[v]--;
+            if (in_degree[v] == 0) q.push(v);
+        }
+
+    }
+
+    if (result.size() < course) {
+        cout << -1 << endl;
+    } 
+    else {
+        for (int i = 0; i < course; i++) {
+            cout << result[i] << (i == course - 1 ? "" : " ");
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
